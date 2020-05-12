7033dd55db89e77a0d2aa777d1e1a37cbad79427
MacFlurry <omegabk@gmail.com>
2020-05-12 23:25:11 +0200

    complete code for provisioning the S3 bucket

---
+++ main.tf
@@ -0,0 +1,75 @@
+# This will be used to set an output value
+data "aws_region" "current" {}
+
+resource "random_string" "rand" {
+  length = 24
+  special = false
+  upper = false
+}
+
+locals {
+  namespace = substr(join("-", [var.namespace, random_string.rand.result]), 0, 24)
+}
+
+# Provides a Resource Group
+resource "aws_resourcegroups_group" "resourcegroups_group" {
+  name = "${local.namespace}-group"
+  # Populate the resource group based on a tagging schema
+  resource_query {
+    query = <<-JSON
+{
+  "ResourceTypeFilters": [
+    "AWS::AllSupported"
+  ],
+  "TagFilters": [
+    {
+      "Key": "ResourceGroup",
+      "Value": ["${local.namespace}"]
+    }
+  ]
+}
+  JSON
+  }
+}
+
+resource "aws_kms_key" "kms_key" {
+  tags {
+    ResourceGroup = local.namespace
+  }
+}
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "${local.namespace}-state-bucket"
+  force_destroy = var.force_destroy_state
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "aws:kms"
+        kms_master_key_id = aws_kms_key.kms_key.arn
+      }
+    }
+  }
+
+  tags {
+    ResourceGroup = local.namespace
+  }
+}
+
+resource "aws_dynamodb_table" "dynamodb_table" {
+  hash_key = "LockID"
+  name = "${local.namespace}-state-lock"
+  # Provision a serverless database for state locking
+  billing_mode = "PAY_PER_REQUEST"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  tags {
+    ResourceGroup = local.namespace
+  }
+}
